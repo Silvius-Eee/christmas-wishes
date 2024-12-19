727e913938832b0bb9944943bc365bb16d53d167
Silvius-Eee <3020602206@qq.com>
2024-12-20 02:15:53 +0800

    修复 TypeScript 类型错误

--- src/views/home/utils/responsive.ts
+++ src/views/home/utils/responsive.ts
@@ -1,4 +1,4 @@
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted, computed } from 'vue'
 
 export function useWindowSize() {
   const width = ref(window.innerWidth)
